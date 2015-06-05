b462a196944c1bd894138abe9b36a27d726663c6
Justin John <ganapati@Justins-MacBook-Air.local>
2015-06-05 07:50:22 -0500

    adding db sesion to Flask app

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -1,8 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect,jsonify, url_for, flash
 from sqlalchemy import create_engine
 from sqlalchemy.orm import session
 
 app = Flask(__name__)
+
+#Connect to Database and create database session
+engine = create_engine('sqlite:///musicdump.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 @app.route('/')
 def HelloWorld():
