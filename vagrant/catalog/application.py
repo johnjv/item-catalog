60d74247e4742471b6da0205868669d6f0b1d0fa
Justin John <ganapati@Justins-Air-2.home>
2015-06-19 22:29:19 -0400

    adding all CRUD methods for application

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -29,12 +29,107 @@ def genresJSON():
     genres = session.query(Genre).all()
     return jsonify(genres = [i.serialize for i in genres])
 
-# Show all of music dump
+# Show all genres
 @app.route('/')
-def homepage():
+def showGenres():
 	songs = session.query(Song).order_by(asc(Song.name))
 	# return jsonify(songs = [i.serialize for i in songs])
 	return render_template('songs.html', songs = songs)
+
+# Create a new music genre
+@app.route('/genre/new', methods=['GET','POST'])
+def newGenre():
+	if request.method == 'POST':
+		newGenre = Genre(name = request.form['name'])
+		session.add(newGenre)
+		flash('Succesfully added %s genre' % newGenre.name)
+		session.commit()
+		return redirect(url_for('showGenres'))
+	else:
+		return render_template('new-genre.html')
+
+# Edit a genre
+@app route('/genre/<int:genre_id>/edit/', methods = ['GET', 'POST'])
+def editGenre(genre_id):
+	editedGenre = session.query(Genre).filter_by(id = genre_id).one()
+	if request.method == 'POST':
+		if request.form['name']:
+			editedGenre.name = request.form['name']
+			flash('Genre successfully edited %s', % editedGenre.name)
+			return redirect(url_for('showGenres'))
+		else:
+			return render_template('edit-genre.html', genre = editedGenre)
+
+# Delete a genre
+@app.route('/genre/<int:genre_id>/delete/', methods = ['GET', 'POST'])
+def deleteGenre(genre_id):
+	genreToDelete = session.query(Genre).filter_by(id = genre_id).one()
+	if request.method = 'POST':
+		session.delete(genreToDelete)
+		flash('%s successfully deleted' % genreToDelete.name)
+		session.commit()
+		return redirect(url_for('showGenres', genre_id = genre_id))
+	else:
+		return render_template('delete-genre.html', genre = genreToDelete)
+
+# Show songs from a genre
+@app.route('/genre/<int:genre_id>/')
+@app.route('/genre/<int:genre_id>/songs/')
+def showSongs(genre_id):
+	genre = session.query(Genre).filter_by(id = genre_id).one()
+	songs = session.genre(Song).filter_by(genre_id = genre_id).all()
+	return render_template(song.html, songs = songs, genre = genre)
+
+# Create a new song for a genre
+@app.route('/genre/<int:genre_id>/songs/new/', methods = ['GET', 'POST'])
+def newSong(genre_id):
+	genre = session.query(Genre).filter_by(id = genre_id).one()
+	if request.method = 'POST':
+		newSong = Song(name = request.form[name],
+					   band_name = request.form[band_name],
+					   country = request.form[country],
+					   youtube_url = request.form[youtube_url])
+		session.add(newSong)
+		session.commit()
+		flash('New song %s successfully created' % (newSong.name))
+		return redirect(url_for('showSongs', genre_id = genre_id))
+	else:
+		return render_template('new-song.html', genre_id = genre_id)
+
+# Edit a song
+@app.route('/genre/<int:genre_id>/song/<int:song_id>/edit', methods = ['GET', 'POST'])
+def editSong(genre_id, song_id):
+	editedSong = session.query(Song).filter_by(id = song_id).one()
+	genre = session.query(Genre).filter_by(id = genre_id).one()
+	if request.method == 'POST':
+		if request.form['name']:
+			editedSong.name = request.form['name']
+		if request.form['band_name']:
+			editedSong.band_name = request.form['band_name']
+		if request.form['country']:
+			editedSong.country = request.form['country']
+		if request.form['youtube_url']:
+			editedSong.youtube_url = request.form['youtube_url']
+		session.add(editedSong)
+		session.commit()
+		flash('Song successfully edited')
+		return redirect(url_for('showSongs', genre_id = genre_id))
+	else:
+		return render_template('edit-song.html', genre_id = genre_id, song_id = song_id, item = editedSong)
+
+# Delete a song
+@app.route('/genre/<int:genre_id/songs/<int:song_id>/delete', methods = ['GET', 'POST'])
+def deleteSong(genre_id, song_id):
+	genre = session.query(Genre).filter_by(id = genre_id).one()
+	songToDelete = session.query(Song).filter_by(id = song_id).one()
+	if request.method = 'POST':
+		session.delete(songToDelete)
+		session.commit()
+		flash('Song successfully deleted')
+		return redirect(url_for('showSong', genre_id = genre_id))
+	else:
+		return render_template('delete-song.html', item = songToDelete)
+
 
 if __name__ == '__main__':
 	app.debug = True
