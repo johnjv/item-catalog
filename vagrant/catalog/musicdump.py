85d23d082eff2cd52818fda70870f3a5d3a4ef0c
Justin John <ganapati@MacBook-Air.home>
2015-08-25 22:10:49 -0400

    adding dummy user to db

--- vagrant/catalog/musicdump.py
+++ vagrant/catalog/musicdump.py
@@ -1,13 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
- 
+
 from database_setup import Genre, Base, Song
- 
+
 engine = create_engine('sqlite:///musicdump.db')
 # Bind the engine to the metadata of the Base class so that the
 # declaratives can be accessed through a DBSession instance
 Base.metadata.bind = engine
- 
+
 DBSession = sessionmaker(bind=engine)
 # A DBSession() instance establishes all conversations with the database
 # and represents a "staging zone" for all the objects loaded into the
@@ -17,6 +17,12 @@ DBSession = sessionmaker(bind=engine)
 # revert all of them back to the last commit by calling
 # session.rollback()
 session = DBSession()
+
+# create a dummy user
+User1 = User(name="Robo Barista", email="tinnyTim@udacity.com",
+             picture='https://pbs.twimg.com/profile_images/2671170543/18debd694829ed78203a5a36dd364160_400x400.png')
+session.add(User1)
+session.commit()
 
 # Looping genre music dump
 loopingGenre = Genre(name = "looping")
