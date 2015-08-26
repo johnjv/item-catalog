7b0b9cebbd40fa8de2731b4b551f3a17500fd5e5
Justin John <ganapati@MacBook-Air.home>
2015-08-25 22:11:33 -0400

    changing local server to port 300

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -264,4 +264,4 @@ def deleteSong(genre_id, song_id):
 if __name__ == '__main__':
 	app.secret_key = "secret key"
 	app.debug = True
-	app.run(host = '0.0.0.0', port = 8000)
+	app.run(host = '0.0.0.0', port = 3000)
