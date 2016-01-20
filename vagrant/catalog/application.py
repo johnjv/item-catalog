be39f4314788471f8411f5299286bc138150b298
Justin Varghese John <justinjohn107@gmail.com>
2016-01-19 19:15:46 -0600

    Fix semi-colons in my python :blush:
    technically works but not PEP8

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -101,7 +101,7 @@ def gconnect():
 
     # Store the access token in the session for later use.
     login_session['provider'] = 'google'
-    login_session['credentials'] = credentials.to_json();
+    login_session['credentials'] = credentials.to_json()
     login_session['gplus_id'] = gplus_id
 
     # Get user info
